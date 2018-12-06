b543ed9d830b547b152dbe9fbbb65cd604e99074
Filippo Campi <filippo.campi@redturtle.it>
2018-12-06 15:53:05 +0100

    [dev] added metadata for taxonomies fields + added upgrade step for catalog

--- src/rt/categorysupport/upgrades.py
+++ src/rt/categorysupport/upgrades.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from plone.app.upgrade.utils import loadMigrationProfile
+from plone import api
+from rt.categorysupport import logger
 
 
-def reload_gs_profile(context):
-    loadMigrationProfile(
-        context,
-        'profile-rt.categorysupport:default'
-    )
+default_profile = 'profile-rt.categorysupport:default'
+
+
+def migrate_to_1001(context):
+    setup_tool = api.portal.get_tool('portal_setup')
+    setup_tool.runImportStepFromProfile(default_profile, 'catalog')
+    logger.info(u'Updated to 1001')
