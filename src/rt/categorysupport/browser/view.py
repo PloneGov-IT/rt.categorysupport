e547c466f2a449229997fb204ce37ae58d394133
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-01-03 11:55:59 +0100

    avoid getObject for brains

--- src/rt/categorysupport/browser/view.py
+++ src/rt/categorysupport/browser/view.py
@@ -7,40 +7,36 @@ class TaxonomyInfo(FolderView):
     """  """
 
     def get_taxonomies(self):
-        if self.context:
-            tax_dict = {}
-            brains = api.content.find(context=self.context)
-            for brain in brains:
-                obj = brain.getObject()
-                if not obj.taxonomies:
-                    continue
-                for tax in obj.taxonomies:
-                    if tax in tax_dict.keys():
-                        tax_dict[tax] += 1
-                    else:
-                        tax_dict[tax] = 1
+        if not self.context:
+            return {}
+        tax_dict = {}
+        brains = api.content.find(context=self.context)
+        for brain in brains:
+            if not brain.taxonomies:
+                continue
+            for tax in brain.taxonomies:
+                if tax in tax_dict:
+                    tax_dict[tax] += 1
+                else:
+                    tax_dict[tax] = 1
 
-            return tax_dict
-
-        return {}
+        return tax_dict
 
     def get_subjects(self):
-        if self.context:
-            _dict = {}
-            brains = api.content.find(context=self.context)
-            for brain in brains:
-                obj = brain.getObject()
-                if not obj.subject:
-                    continue
-                for el in obj.subject:
-                    if el in _dict.keys():
-                        _dict[el] += 1
-                    else:
-                        _dict[el] = 1
+        if not self.context:
+            return {}
+        _dict = {}
+        brains = api.content.find(context=self.context)
+        for brain in brains:
+            if not brain.Subject:
+                continue
+            for el in brain.Subject:
+                if el in _dict.keys():
+                    _dict[el] += 1
+                else:
+                    _dict[el] = 1
 
-            return _dict
-
-        return {}
+        return _dict
 
     def batch(self):
         return super(TaxonomyInfo, self).batch()
